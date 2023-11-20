3975c206c5fdb8f488b3d28eb9ccd3fe12dd1321
Matthew Bolofinde <molanrewajuking@gmail.com>
2023-11-20 14:22:02 -0600

    Added Temperature Lab

---
+++ temprateture values/findTemp.cpp
@@ -0,0 +1,41 @@
+/* 
+
+Write a program that asks users to enter the temperature in their room. The program then
+prints one of the following messages based on the value of temperature.
+Temperature Message
+---------------------------------------------------------------------------------------------------------
+Above than 90 “Visit a neighbor.”
+Lower than or equal to 90, but above than 80 “Turn on air conditioning.”
+Lower than or equal to 80, but greater than 70 “Do nothing.”
+Lower than or equal to 70, but greater than 55 “Turn on heat.”
+Lower than or equal 55 “Visit a neighbor.”
+2- Write a program to calculate employee’s wage. Your program first asks the user to enter the
+number of hours an employee works in one week (a float value). Then, it calculates this
+employee’s wage based on the following formula:
+(1) Hourly pay rate is $18.25 for the first normal 40.0 hours
+(2) An overtime pay-rate factor of 1.5 is applied to the overtime hours
+The wage displayed should have 2 digits fraction part because of the money unit. The “if-
+else” statement is required.
+Handing in your assignment: Upload your individual CPP files to eCourses by the due date.
+Also, upload a screenshot of your console output.
+
+
+*/
+
+
+// SOLUTION 
+
+#include <iostream>
+using   namespace std; 
+
+int main ()
+
+ {
+
+
+
+
+
+
+
+}
